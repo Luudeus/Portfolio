3d830289c3291cb16b55ccc1f6bc57c20b058146
Luudeus <lacabrales@gmail.com>
2025-07-24 21:32:41 -0400

    Fix mobile view

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Luciano André Cabrales Lefimil';
-export const SITE_DESCRIPTION = 'Luciano André Cabrales Lefimil - Desarrollador Full Stack: Técnico en Programación y Análisis de Sistemas especializado en desarrollo web, integración de APIs, automatización y soluciones tecnológicas personalizadas desde La Serena, Chile.';
+export const SITE_DESCRIPTION = 'Luciano André Cabrales Lefimil - Desarrollador Full Stack: Técnico en Programación y Análisis de Sistemas especializado en desarrollo web, integración de APIs, automatización y soluciones tecnológicas personalizadas desde Santiago, Chile.';
